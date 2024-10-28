ddb4ef1b28fe72a226e1760aa4fbd2f7f8a2f135
Ian M <50029473+ianm199@users.noreply.github.com>
2024-10-27 20:39:19 -0400

    Delete beeminder_client directory

--- beeminder_client/__init__.py
+++
@@ -1,7 +0,0 @@
-"""A Python client library and terminal interface for Beeminder."""
-
-from .beeminder import BeeminderAPI
-from .models import BeeminderGoal, Datapoint, Contract
-
-__version__ = "0.1.0"
-__all__ = ['BeeminderAPI', 'BeeminderGoal', 'Datapoint', 'Contract']
